8460b4b1ee524d0c937eefcc2b10441ee8fc3d6a
karan <ashiwalkaran@gmail.com>
2023-02-23 21:15:34 +0530

    4th->accessing amount state in navbar.js ,using dispatch hook

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import {actionCreators} from './state/index'
+
 
 const Shop = () => {
+    const dispatch=useDispatch();
   return (
     <>
         <h2>Buy Something</h2>
-        <button className="btn btn-primary mx-2">-</button>
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
         Add to cart
-        <button className="btn btn-primary mx-2">+</button>
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
     </>
   )
 }
