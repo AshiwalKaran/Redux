2b45fa824362f3b08d90e62ae1c0182413c5c891
karan <ashiwalkaran@gmail.com>
2023-02-23 21:26:08 +0530

    5th->Bind Action creator to short the code

--- src/Shop.js
+++ src/Shop.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux';
 import {actionCreators} from './state/index'
 
-
+// useSelector is used to access state
+//useDispatch is used to access action creators
 const Shop = () => {
     const dispatch=useDispatch();
+    const amount=useSelector((state)=>{return state.amount});
+    const {withdrawMoney,depositMoney}=bindActionCreators(actionCreators,dispatch);
+
+
   return (
     <>
         <h2>Buy Something</h2>
-        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
-        Add to cart
-        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
+        <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+        Updated Balance is ({amount})
+        <button className="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
     </>
   )
 }
