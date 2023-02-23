a79d97b8e13d38b52e8f4e7bc7cd5361d5ba1ec5
karan <ashiwalkaran@gmail.com>
2023-02-23 17:26:54 +0530

    1st-->Action-creator and reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type='button' className='btn btn-primary' disabled={true}>Your Balance : 0</button>
+      <Navbar/>
+      <Shop/>
+    </>
   );
 }
 
